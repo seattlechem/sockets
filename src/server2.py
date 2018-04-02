38295946530132c59586bbb1e4eeef83e4e02e44
Peter Kim <seattlechem@yahoo.com>
2018-04-02 15:09:38 -0700

    comments are removed from server.py

--- src/server2.py
+++ src/server2.py
@@ -3,14 +3,25 @@ import system
 
 
 def listen():
+    # create TCP/IP socket
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # bind the socket to the port
     connection.bind(('127.0.0.1', 3000))
+    # calling listen() puts the socket into server mode accept() waits for \
+    # incoming connection
     connection.listen(10)
     now = system.datetime.datetime.now()
     print('Starting server on port 3000 at {}'.format(now))
     while True:
+        # wait for connection
         current_connection, address = connection.accept()
+        '''
+        accept() returns an open connection between the server and client, \
+        along with the address of the client. The connection is actually a \
+        different socket on another port (assigned by the kernel). Data is \
+        read from the connection with recv() and transmitted with sendall().
+         '''
         while True:
             data = current_connection.recv(2048)
 
